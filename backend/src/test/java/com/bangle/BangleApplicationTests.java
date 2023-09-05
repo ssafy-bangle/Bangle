f40a62dc9fdce4e85b633635f7e3a8c7b0812e33
윤유탁 <dbsdbxkrzz@naver.com>
2023-09-05 10:32:48 +0900

    Initial commit

---
+++ backend/src/test/java/com/bangle/BangleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bangle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
