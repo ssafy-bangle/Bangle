feb9dcbf3a11c1164e0edfe6bc185aaa49c157f3
조원주 <dnjswn9178@hanmail.net>
2023-09-12 15:29:58 +0900

    feat: add modal components

---
+++ frontend/src/components/molecules/modal/Modal.stories.tsx
@@ -0,0 +1,48 @@
+import { StoryObj, Meta } from '@storybook/react';
+import Modal from '.';
+
+export default {
+  component: Modal,
+} as Meta<typeof Modal>;
+
+type Story = StoryObj<typeof Modal>;
+
+const Template: Story = {
+  name: 'Default',
+  argTypes: {
+    open: {
+      type: 'boolean',
+      description: '여부',
+    },
+    title: {
+      type: 'string',
+      description: '제목',
+    },
+    type: {
+      type: 'string',
+      description: '텍스트',
+    },
+    firstPrice: {
+      type: 'number',
+      description: '가격1',
+    },
+    secondPrice: {
+      type: 'number',
+      description: '가격2',
+    },
+  },
+  render: (args) => <Modal {...args} />,
+};
+
+export const DefaultModal: Story = {
+  ...Template,
+  name: 'DefaultModal',
+  args: {
+    ...Template.args,
+    open: false,
+    type: 'publish',
+    title: '테스트케이스',
+    firstPrice: 5,
+    secondPrice: 10,
+  },
+};
---
+++ frontend/src/components/molecules/modal/index.tsx
@@ -0,0 +1,62 @@
+import * as S from './index.styled';
+import { useState } from 'react';
+import { ModalProps } from '@src/types/props';
+import Image from 'next/image';
+import { DarkMunzi } from '@src/assets/imgs';
+import Button from '@src/components/atoms/button';
+
+export default function Modal({ open, type, title, firstPrice, secondPrice, onClick }: ModalProps) {
+  const [isOpen, setOpen] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleOk = (e: React.MouseEvent<HTMLElement>) => {
+    console.log(e);
+    setOpen(false);
+  };
+
+  const handleCancel = (e: React.MouseEvent<HTMLElement>) => {
+    console.log(e);
+    setOpen(false);
+  };
+  return (
+    <>
+      <S.StyledButton type="primary" onClick={showModal}>
+        Open Modal with customized button props
+      </S.StyledButton>
+      <S.StyledModal
+        open={isOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okButtonProps={{ disabled: true }}
+        cancelButtonProps={{ disabled: true }}
+        modalRender={() => (
+          <S.StyledContainer>
+            <S.StyledTitle>{title}</S.StyledTitle>
+            <S.Divider />
+            <S.MunziContainer>
+              <S.FirstMunzi>
+                보유먼지
+                <S.PriceContainer>
+                  <Image src={DarkMunzi} alt="다크먼지" width={30} />
+                  <S.MunziPrice>{firstPrice}</S.MunziPrice>
+                </S.PriceContainer>
+              </S.FirstMunzi>
+              <S.FirstMunzi>
+                구매먼지
+                <S.PriceContainer>
+                  <Image src={DarkMunzi} alt="다크먼지" width={30} />
+                  <S.MunziPrice>{secondPrice}</S.MunziPrice>
+                </S.PriceContainer>
+              </S.FirstMunzi>
+            </S.MunziContainer>
+            <S.Divider />
+            <Button theme="text" content="장바구니 담기" icon="cart" length="long" onClick={() => {}} />
+            <Button theme="default" content="즉시구매(10먼지)" length="long" onClick={() => {}} />
+          </S.StyledContainer>
+        )}></S.StyledModal>
+    </>
+  );
+}
