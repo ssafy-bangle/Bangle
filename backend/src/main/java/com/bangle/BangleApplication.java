0d2cf93e7595765aa01f98c636c3b7f37eeaabee
김동훈 <hunn000@gmail.com>
2023-09-12 16:13:33 +0900

    fix: dockerfile to gradle clean build

--- backend/src/main/java/com/bangle/BangleApplication.java
+++ backend/src/main/java/com/bangle/BangleApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BangleApplication {
 
-	public static void main(String[] args) {SpringApplication.run(BangleApplication.class, args);}
+	public static void main(String[] args) {
+		SpringApplication.run(BangleApplication.class, args);
+		System.out.println("VERSION999=======VERSION999=======VERSION999=======VERSION999=======VERSION999=======VERSION999=======VERSION999=======");
+	}
 
 }
