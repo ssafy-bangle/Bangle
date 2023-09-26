a70d1972ec2507365ab63c5da1abba72cf6ffa47
HongJaeyeon <ddrrff@kookmin.ac.kr>
2023-09-26 13:43:35 +0900

    fix: roles bug

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,9 +1,9 @@
 import { useRecoilValue } from 'recoil';
-import { UserInfoState } from '@src/modules/user';
+import { UserModeState } from '@src/modules/user';
 import UserHome from '@src/components/organisms/userHome';
 import AuthorHome from '@src/components/organisms/authorHome';
 
 export default function Home() {
-  const { roles } = useRecoilValue(UserInfoState);
-  return <>{roles === 'ROLE_USER' ? <UserHome /> : <AuthorHome />}</>;
+  const mode = useRecoilValue(UserModeState);
+  return <>{mode === 'user' ? <UserHome /> : <AuthorHome />}</>;
 }
