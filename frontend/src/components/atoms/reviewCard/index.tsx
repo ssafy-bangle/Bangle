eb922bbd396554c36fb76dbb2a785ec491c82d8d
조원주 <dnjswn9178@gmail.com>
2023-09-29 22:02:29 +0900

    feat: book detail and review card

--- frontend/src/components/atoms/reviewCard/index.tsx
+++ frontend/src/components/atoms/reviewCard/index.tsx
@@ -1,10 +1,16 @@
 import { BookCoverProps } from '@src/types/props';
 import * as S from './index.styled';
+import { useState } from 'react';
 
 export default function ReviewCard({ imgsrc, size }: BookCoverProps) {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const setHoverHandler = () => {
+    setIsHovered((pre) => !pre)
+  };
   return (
-    <S.ImgContainer size={size} imgsrc={imgsrc}>
-      <S.Img src={imgsrc} alt="bookCoverImg" width={300} height={400} ></S.Img>
+    <S.ImgContainer size={size} imgsrc={imgsrc} onMouseEnter={setHoverHandler} onMouseLeave={setHoverHandler}>
+      <S.Img src={imgsrc} alt="bookCoverImg" width={300} height={400} />
+      <S.ButtonOverlay isHovered={isHovered} onClick={() => {}} >리뷰 보기</S.ButtonOverlay>
     </S.ImgContainer>
   );
 }
