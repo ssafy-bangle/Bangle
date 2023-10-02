3f9467765e4a7427b466ba9c94f93e4990dbf142
영서 <kys981207@naver.com>
2023-10-02 22:31:04 +0900

    fix: 오늘의 통계를 매일 오전 12시에 삭제하도록 구현

--- backend/src/main/java/com/bangle/global/config/DeleteRedisDataTask.java
+++ backend/src/main/java/com/bangle/global/config/DeleteRedisDataTask.java
@@ -12,11 +12,8 @@ public class DeleteRedisDataTask {
 
     private final RedisTemplate<String, String> template;
 
-    @Scheduled(cron = "1 * * * * *") // 매일 오전 3시에 실행
+    @Scheduled(cron = "0 0 * * * *") // 매일 오전 12시에 실행
     public void deleteRedisData() {
-        // Redis 데이터 삭제 로직을 여기에 작성
-        // template을 사용하여 데이터를 삭제
-        // 주기적인 스케줄링으로 이 메서드가 실행될 때마다 Redis 데이터가 삭제됨
         String keyPattern = "*today*";
         Set<String> keysToDelete = template.keys(keyPattern);
         template.delete(keysToDelete);
