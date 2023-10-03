1f601985b48b63e2333c17f3450242e3317904f9
hun23 <hunn2023@gmail.com>
2023-10-03 18:45:20 +0900

    fix: add constructor for bookIdAddressReseponse

--- backend/src/main/java/com/bangle/domain/book/dto/BookIdAddressResponse.java
+++ backend/src/main/java/com/bangle/domain/book/dto/BookIdAddressResponse.java
@@ -1,7 +1,15 @@
 package com.bangle.domain.book.dto;
 
 import com.bangle.domain.bookshelf.entity.Bookshelf;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookIdAddressResponse {
     Long id;
     String address;
