24cd28711ffc926367f31406a5033b688633ffb6
조원주 <dnjswn9178@hanmail.net>
2023-10-04 23:46:28 +0900

    feat: add genre interests

--- frontend/src/types/props.ts
+++ frontend/src/types/props.ts
@@ -117,7 +117,7 @@ export type BooksContainerProps = {
   type?: 'book' | 'author';
   page: 'bookShelf' | 'search' | 'wishList';
   data?: SearchBook | any; //작가의 content type이 api 명세서에 없는 이슈로 any로 임시 지정
-  title: string;
+  title?: string;
   onClick?: (address: string | undefined) => void;
 };
 
