79cbc4ce22f38a6846515d9b763a7a9704078fe8
조원주 <dnjswn9178@hanmail.net>
2023-10-04 23:45:24 +0900

    feat: add subscribe author

--- frontend/src/components/organisms/userHome/index.tsx
+++ frontend/src/components/organisms/userHome/index.tsx
@@ -6,15 +6,22 @@ import Carousel from '@src/components/atoms/carousel';
 import { useRecoilValue } from 'recoil';
 import { UserInfoState } from '@src/modules/user';
 import { useEffect, useState } from 'react';
-import { bookApi } from '@src/apis';
+import { authorApi, bookApi } from '@src/apis';
+import BooksContainer from '../booksContainer';
 
 export default function UserHome() {
   const recoilUserInfo = useRecoilValue(UserInfoState);
   const [nickname, setNickname] = useState<string>('');
+  const [subAuthorList, setSubAuthorList] = useState<{id: number, nickname: string}[]>([]);
+
   useEffect(() => {
     setNickname(recoilUserInfo.nickname);
+    authorApi.getSubscribeAuthor().then((res) => {
+      setSubAuthorList(() => [...res.data]);
+    });
     // bookApi.getGenre();
   }, []);
+
   return (
     <>
       <S.BannerSection>
@@ -22,7 +29,7 @@ export default function UserHome() {
           오늘의 <strong>책</strong>
         </S.Title>
         <S.ContainerBox>
-          <Image width={550} src={BannerImg} alt="bannerImg" />
+          <Image width={550} src={BannerImg} alt="bannerImg" quality={100} />
           <S.BookBox>
             <Image width={120} src={BookImg} alt="BookImg" />
             <Button length="long" content="구매하기" />
@@ -34,6 +41,10 @@ export default function UserHome() {
           <strong>{nickname}</strong>님을 위한 <strong>맞춤</strong>추천
         </S.Title>
         <Carousel />
+      </S.RecommendSection>
+      <S.RecommendSection>
+        <S.Title><strong>{nickname}</strong>님의 관심 작가 목록</S.Title>
+        <BooksContainer type="author" page="bookShelf" data={subAuthorList} />
       </S.RecommendSection>
     </>
   );
