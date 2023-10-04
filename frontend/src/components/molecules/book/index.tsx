379a064f420a7e0427023e96cd0b6c19a30569a5
조원주 <dnjswn9178@hanmail.net>
2023-10-04 11:01:41 +0900

    feat: add wishList and bookTitle, router

--- frontend/src/components/molecules/book/index.tsx
+++ frontend/src/components/molecules/book/index.tsx
@@ -2,11 +2,11 @@ import * as S from './index.styled';
 import { BookProps } from '@src/types/props';
 import BookCover from '@src/components/atoms/bookCover';
 
-export default function Book({ imgsrc, onClick }: BookProps) {
+export default function Book({ data, imgsrc, onClick }: BookProps) {
   return (
     <div>
       <BookCover imgsrc={imgsrc} onClick={onClick} />
-      <S.BookTitle onClick={onClick}>BookTitle</S.BookTitle>
+      <S.BookTitle onClick={onClick}>{data?.title}</S.BookTitle>
     </div>
   );
 }
